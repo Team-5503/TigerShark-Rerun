484aaeac68c5a90704ebe275c43605ddbfde1897
Matthew Ferrell <78511776+MatthewFerrell@users.noreply.github.com>
2025-03-04 16:09:40 -0600

    All subsystem files created
    need to be initialized

---
+++ src/main/java/frc/robot/commands/climb/climbStop.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.climb;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Climber;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class climbStop extends Command {
+private final Climber climb;
+  /** Creates a new climbReach. */
+  public climbStop(Climber subsystem) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    climb = subsystem;
+    addRequirements(climb);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    climb.stop();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
